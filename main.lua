28f5db15e5b4593e94959314960a995bb0ca27cb
rileymulligan <123499121+rileymulligan@users.noreply.github.com>
2023-04-03 17:00:38 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 function love.load()
+-- this runs once, when the project is first loaded
+
+
+-- setting up the x and y values
   x = 50
   y = 30
 
@@ -11,27 +15,33 @@ function love.load()
 end
 
 function love.update(dt)
+-- this runs as part of the game's update loop
+  -- you can think of this as being called once
+  -- per frame.  Put stuff in here that will
+  -- update the state of your game variables
 
+  -- right movement
   if love.keyboard.isDown("right") and x < width - 110 then
     x = x + speed * dt
   end
 
+    -- left movement
   if love.keyboard.isDown("left") then
     x = x - speed * dt
   end
 
+    -- up movement
   if love.keyboard.isDown("up") then
     y = y - speed * dt
   end
 
+  -- down movement
   if love.keyboard.isDown("down") then
     y = y + speed * dt
   end
-
 end
 
 function love.draw(dt)
-
+    -- draw the rectangle
     love.graphics.rectangle("line", x, y, 100, 200)
-
 end
