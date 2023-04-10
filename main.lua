36b62ecd3d5fc0fe43d360e8892077c6b0ee2aa4
rileymulligan <123499121+rileymulligan@users.noreply.github.com>
2023-04-10 16:52:10 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -5,11 +5,13 @@ function love.load()
 -- setting up the x and y values
   x = 50
   y = 30
+  w = 30
+  h = 30
 
-  speed = 150
+  speed = 500
 
   width = love.graphics.getWidth()
-  height = love.graphics.getWidth()
+  height = love.graphics.getHeight()
 
 
 end
@@ -21,27 +23,31 @@ function love.update(dt)
   -- update the state of your game variables
 
   -- right movement
-  if love.keyboard.isDown("right") and x < width - 110 then
+  if love.keyboard.isDown("right") and x < width - w then
     x = x + speed * dt
   end
 
     -- left movement
-  if love.keyboard.isDown("left") then
+  if love.keyboard.isDown("left") and x > 0 then
     x = x - speed * dt
   end
 
     -- up movement
-  if love.keyboard.isDown("up") then
+  if love.keyboard.isDown("up") and y > 0  then
     y = y - speed * dt
   end
 
   -- down movement
-  if love.keyboard.isDown("down") then
+  if love.keyboard.isDown("down") and y < height - h then
     y = y + speed * dt
   end
+
+
+
+
 end
 
 function love.draw(dt)
     -- draw the rectangle
-    love.graphics.rectangle("line", x, y, 100, 200)
+    love.graphics.rectangle("line", x, y, w, h)
 end
